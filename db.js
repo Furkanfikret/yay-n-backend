48bfdcd5448403b0643b53319f38be01db8fbc35
Furkanfikret <dedefiko1111@gmail.com>
2025-07-13 18:28:56 +0300

    ikinci commit

--- db.js
+++ db.js
@@ -1,1 +1,24 @@
-import pg from 'pg';
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {Pool} = pkg;
+const pool = new Pool({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
+})
+
+export const dbTest =async () => {
+    try {
+        const res = await pool.query('select now()');
+        console.log(`Veri tabanı bağlantısı başarılı ! `)
+    } catch (error) {
+        console.log(`Veri tabanı bağlantı Hatası ! - ${error}`);
+    }
+}
+
+export default pool;
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import pool,{dbTest} from './db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get("/",(req,res) =>{
-    res.send("<h1>Main Page Of Backend !</h1>");
+    res.sendFile(path.join(__dirname,'views','home_page.html'));
 })
 app.listen(port,()=>{
     console.log(`Backend Server Is Running On Port ${port} !`);
+    dbTest();
 })
 
